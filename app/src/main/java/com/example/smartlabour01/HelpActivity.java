4ff399baaf2e4e4c544059cf9d9997ae70ac116c
ansarimasood <ansarimasood85@gmail.com>
2019-02-11 12:45:35 +0530

    pverify activity updated login and signup updated

--- app/src/main/java/com/example/smartlabour01/HelpActivity.java
+++ app/src/main/java/com/example/smartlabour01/HelpActivity.java
@@ -12,7 +12,7 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.hlptoolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Help");
         setSupportActionBar(toolbar);
 
